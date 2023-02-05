3fd87874e33b1157f11cfa53e0c1c4a161915f5f
cevher.bozkur <bozkur@gmail.com>
2023-02-05 22:08:12 +0300

    Gömülü ActiveMQ sunucusu iptal edilid. Mesajların yerelde kuruluna gitmesi sağlandı.

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,23 +1,20 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgJmsApplication {
 
-    public static void main(String[] args) throws Exception {
-        ConfigurationImpl configuration = new ConfigurationImpl()
+    public static void main(String[] args) {
+       /* ConfigurationImpl configuration = new ConfigurationImpl()
                 .setPersistenceEnabled(false)
                 .setJournalDirectory("target/data/journal")
                 .setSecurityEnabled(false)
                 .addAcceptorConfiguration("invm", "vm://15000");
         //ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(configuration);
         ActiveMQServer activeMQServer = new ActiveMQServerImpl(configuration);
-        activeMQServer.start();
+        activeMQServer.start();*/
         SpringApplication.run(SfgJmsApplication.class, args);
     }
 
